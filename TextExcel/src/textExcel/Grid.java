c376216491762e4a8fbe637788437fb9ce2c0202
Jeffrey Ding <123779994+jeffreyhding@users.noreply.github.com>
2023-05-15 08:41:08 -0700

    Update Grid.java

--- TextExcel/src/textExcel/Grid.java
+++ TextExcel/src/textExcel/Grid.java
@@ -1,12 +1,7 @@
 package textExcel;
 
-//*******************************************************
-//DO NOT MODIFY THIS FILE!!!
-//*******************************************************
-
 public interface Grid 
 {
-	// Grid interface, must be implemented by your Spreadsheet class
 	String processCommand(String command); // processes a user command, returns string to display, must be called in loop from main
 	int getRows(); // returns number of rows in grid
 	int getCols(); // returns number of columns in grid
