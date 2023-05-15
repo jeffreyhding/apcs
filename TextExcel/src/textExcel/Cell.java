ea934d6c27c1a51968c6f7e77c72c698fa9a0fa3
Jeffrey Ding <123779994+jeffreyhding@users.noreply.github.com>
2023-05-15 08:41:32 -0700

    Update Cell.java

--- TextExcel/src/textExcel/Cell.java
+++ TextExcel/src/textExcel/Cell.java
@@ -1,5 +1,10 @@
 package textExcel;
 
+/** 
+ * @author Jeffrey Ding
+ * @version 02/23/2023
+ */ 
+
 public interface Cell
 {
 	public String abbreviatedCellText(); // text for spreadsheet cell display, must be exactly length 10
